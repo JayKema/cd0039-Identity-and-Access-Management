64df4ff806fcfe4b5138c33e7136b5f4db27e6d2
Jeremiah Nwosu <jeremiahfelix.n@gmail.com>
2022-09-06 07:25:29 +0100

    Add: udacity coffee shop project

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'kema.us.auth0.com', // the auth0 domain prefix
+    url: 'kema', // the auth0 domain prefix
     audience: 'firstApi', // the audience set for the auth0 app
     clientId: '2RlGtKAGsmFKrayr6Clq2w7CWcUA8DQj', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
